78b56bf6c6c82d083db035f9e3dd2d1e371aa8f8
vicryf <vicryf@gmai.com>
2018-04-23 19:45:34 +0700

    BottomNavigationBar sudah tidak mengoverlay menu lainnya.

--- app/src/main/java/com/keyuni/android/bullet/ProfilActivity.java
+++ app/src/main/java/com/keyuni/android/bullet/ProfilActivity.java
@@ -1,7 +1,14 @@
 package com.keyuni.android.bullet;
 
+import android.annotation.SuppressLint;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Field;
 
 public class ProfilActivity extends AppCompatActivity {
 
@@ -9,5 +16,27 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        BottomNavigationView btm = findViewById(R.id.bottomNavView);
+        disableShiftMode(btm);
+    }
+    @SuppressLint("RestrictedApi")
+    private void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
     }
 }
